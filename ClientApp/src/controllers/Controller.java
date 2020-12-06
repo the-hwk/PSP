23aba6c984401fbb1a59bc242ee4ae3c349551fc
Yastrebov Alexey <kiborg_4@mail.ru>
2020-12-06 15:56:55 +0300

    Added task photos

--- ClientApp/src/controllers/Controller.java
+++ ClientApp/src/controllers/Controller.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import javafx.scene.web.WebEngine;
+import netscape.javascript.JSObject;
 
 public class Controller {
     private final WebEngine engine;
@@ -10,12 +11,19 @@ public class Controller {
     }
 
     public void createConnection() {
-        engine.load(this.getClass().getResource("views/main.html").toExternalForm());
+        System.out.println("Connection");
+        engine.load(this.getClass().getResource("../views/main.html").toExternalForm());
+        JSObject windowObject = (JSObject) engine.executeScript("window");
+        windowObject.setMember("app", this);
         /*
         if (Connection.createConnection(host)) {
             engine.load(this.getClass().getResource("views/auth.html").toExternalForm());
         } else {
             // TODO: JS Error
         }*/
+    }
+
+    public void check() {
+        System.out.println("Check");
     }
 }
